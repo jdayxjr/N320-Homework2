762d481b5934c030baf37c99b2bc54877859d182
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2022-09-12 17:24:48 -0400

    first push

---
+++ app.js
@@ -0,0 +1,33 @@
+class instrument {
+
+    constructor(loudness, family, play) {
+        this.loudness = loudness;
+        this.family = family;
+        this.play = play;
+    }
+
+}
+
+let pInstrument = {
+    duration: '3',
+
+    function() {
+
+        console.log(instrument);
+    }
+
+}
+
+class woodwind {
+
+
+}
+
+class percussion {
+
+
+}
+
+class string {
+
+}
